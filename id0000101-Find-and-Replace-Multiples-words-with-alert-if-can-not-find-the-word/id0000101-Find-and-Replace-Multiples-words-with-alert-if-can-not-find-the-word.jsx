f7edc82a66571c6e4d366ec651c780b4d082a1b9
firedevelop <firedevelop.com@gmail.com>
2018-06-12 14:10:32 +0200

    .
    .

---
+++ id0000101-Find-and-Replace-Multiples-words-with-alert-if-can-not-find-the-word/id0000101-Find-and-Replace-Multiples-words-with-alert-if-can-not-find-the-word.jsx
@@ -0,0 +1,17 @@
+replaceTextUsingFIND ("Article 1.", "Article ONE");     
+replaceTextUsingFIND ("Article 2.", "Article TWO");   
+replaceTextUsingFIND ("Article 3.", "Article THREE");   
+replaceTextUsingFIND ("Article 4.", "Article FOUR");  
+
+function replaceTextUsingFIND (input, output)    
+{    
+    app.findTextPreferences = app.changeTextPreferences = NothingEnum.nothing;    
+    app.findTextPreferences.findWhat = input;    
+    app.changeTextPreferences.changeTo = output;    
+    var changedResults = app.activeDocument.changeText();    
+    app.findTextPreferences = app.changeTextPreferences = NothingEnum.nothing;     
+
+    if ( changedResults == 0)  { 
+        alert ( "No results for " + input);
+    }  
+}
