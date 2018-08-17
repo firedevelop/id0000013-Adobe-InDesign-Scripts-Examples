7f7b416e2e95ad14cbc2813058547356d198d843
firedevelop <firedevelop.com@gmail.com>
2018-08-17 13:41:46 +0200

    .
    .

---
+++ id0000119-Translation-Index-Reference-Find-Replace-text/id0000119-Translation-Index-Reference-Find-Replace-text.jsx
@@ -0,0 +1,21 @@
+﻿(function () {  
+    var doc = app.activeDocument,  
+        indexIdx = doc.indexes.length - 1;  
+          
+    function removeNewEntriesFromIndex(index) {  
+        var idx = index.topics.length - 1;  
+          
+        for (; idx >= 0; idx -= 1) {  
+            $.writeln(idx);
+
+            index.topics[idx].name = index.topics[idx].name.replace(/ *^wisdom$/, 'sabiduría');
+            index.topics[idx].name = index.topics[idx].name.replace(/ *^wisdom realizing knowledge$/, 'sabiduría que realiza el conocimiento');
+            index.topics[idx].name = index.topics[idx].name.replace(/ *^realizing knowledge$/, 'que realiza el conocimiento');
+            index.topics[idx].name = index.topics[idx].name.replace(/ *^knowledge$/, 'vacuidad');
+        }  
+    }  
+      
+    for (; indexIdx >= 0; indexIdx -= 1) {  
+        removeNewEntriesFromIndex(doc.indexes[indexIdx]);  
+    }  
+}()); 
